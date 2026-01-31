6ce9a4d6a2b6b1fcdc068eb8ccfb95d95f5c25b3
playgamesgo <pplaygamesgo@gmail.com>
2026-02-01 01:29:58 +0200

    Add raw PVP status placeholder and Minimessage support for placeholders

--- src/main/java/me/playgamesgo/pvptoggles/PVPToggles.java
+++ src/main/java/me/playgamesgo/pvptoggles/PVPToggles.java
@@ -3,6 +3,9 @@ package me.playgamesgo.pvptoggles;
 import dev.rollczi.litecommands.fabric.LiteFabricFactory;
 import dev.rollczi.litecommands.fabric.server.FabricServerSender;
 import dev.rollczi.litecommands.permission.PermissionDefaultResolver;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import me.playgamesgo.pvptoggles.commands.PVPAdminCommand;
 import me.playgamesgo.pvptoggles.commands.PVPToggleCommand;
@@ -10,7 +13,9 @@ import me.playgamesgo.pvptoggles.utils.Config;
 import me.playgamesgo.pvptoggles.utils.PacketRegistry;
 import me.playgamesgo.pvptoggles.utils.PlaceholdersUtil;
 import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
+import net.kyori.adventure.platform.modcommon.MinecraftServerAudiences;
 import net.kyori.adventure.text.Component;
 import net.minecraft.command.DefaultPermissions;
 import net.minecraft.server.command.ServerCommandSource;
@@ -19,8 +24,12 @@ public final class PVPToggles implements DedicatedServerModInitializer {
     public static final String MOD_ID = "pvptoggles";
     public static final boolean isEclipseCustomNameLoaded = FabricLoader.getInstance().isModLoaded("eclipsescustomname");
 
+    @Getter @Setter(AccessLevel.PRIVATE) private static volatile MinecraftServerAudiences adventure;
+
     @Override
     public void onInitializeServer() {
+        ServerLifecycleEvents.SERVER_STARTING.register(minecraftServer -> setAdventure(MinecraftServerAudiences.of(minecraftServer)));
+
         Config.HANDLER.load();
         PlaceholdersUtil.init();
         PacketRegistry.registerPackets();
--- src/main/java/me/playgamesgo/pvptoggles/utils/PlaceholdersUtil.java
+++ src/main/java/me/playgamesgo/pvptoggles/utils/PlaceholdersUtil.java
@@ -1,25 +1,34 @@
 package me.playgamesgo.pvptoggles.utils;
 
+import eu.pb4.placeholders.api.PlaceholderContext;
 import eu.pb4.placeholders.api.PlaceholderResult;
 import eu.pb4.placeholders.api.Placeholders;
 import me.playgamesgo.pvptoggles.PVPToggles;
 import me.playgamesgo.pvptoggles.mixinaccess.IPVPEntity;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 
 public final class PlaceholdersUtil {
     public static void init() {
-        Placeholders.register(Identifier.of(PVPToggles.MOD_ID, "pvpstatus"), (ctx, arg) -> {
-            if (!ctx.hasPlayer()) return PlaceholderResult.invalid("No player!");
+        Placeholders.register(Identifier.of(PVPToggles.MOD_ID, "pvpstatus"), (ctx, arg) -> parsePlaceholder(ctx, false));
+        Placeholders.register(Identifier.of(PVPToggles.MOD_ID, "pvpstatus_raw"), (ctx, arg) -> parsePlaceholder(ctx, true));
+    }
 
-            Config config = Config.HANDLER.instance();
-            ServerPlayerEntity player = ctx.player();
-            IPVPEntity pvpEntity = (IPVPEntity) player;
+    private static PlaceholderResult parsePlaceholder(PlaceholderContext ctx, boolean raw) {
+        if (!ctx.hasPlayer()) return PlaceholderResult.invalid("No player!");
 
-            return PlaceholderResult.value(pvpEntity.PVPToggles$isPVPEnabled()
+        Config config = Config.HANDLER.instance();
+        ServerPlayerEntity player = ctx.player();
+        IPVPEntity pvpEntity = (IPVPEntity) player;
+
+        if (raw) {
+            PlaceholderResult.value(pvpEntity.PVPToggles$isPVPEnabled()
+                    ? PVPToggles.getAdventure().asNative(MiniMessage.miniMessage().deserialize(config.getPvpEnabledPlaceholder()))
+                    : PVPToggles.getAdventure().asNative(MiniMessage.miniMessage().deserialize(config.getPvpDisabledPlaceholder())));
+        }
+        return PlaceholderResult.value(pvpEntity.PVPToggles$isPVPEnabled()
                 ? config.getPvpEnabledPlaceholder()
                 : config.getPvpDisabledPlaceholder());
-        });
-
     }
 }
